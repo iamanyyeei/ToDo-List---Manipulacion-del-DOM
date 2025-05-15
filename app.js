0de46bb1529594bce7285493db457d5f7646b025
iamanyyeei <iamanyyeei@gmail.com>
2025-05-15 18:27:29 -0400

    Creación de nuevas tareas, editar y eliminar tareas de la lista

--- app.js
+++ app.js
@@ -19,5 +19,86 @@ taskForm.addEventListener("submit", (event) =>{
     const task = taskInput.value;
     //Muestra dicho valor para confirmar que el código funciona hasta acá
     console.log(task);
+
+    //Valida si es verdadero para agregar una nueva tarea
+    if(task){
+        //Agrega con APPEND la nueva tarea después de la tarea por default
+        //Para esto se llama a la función createTaskElement
+        taskList.append(createTaskElement(task));
+        taskInput.value = "";
+    }
     
 });
+
+
+//CREAR NUEVAS TAREAS
+
+function createTaskElement(task){
+    //Selecciona un elemento padre li para crear la nueva tarea
+    const li = document.createElement("li");
+    //Crear el elemento p (donde se agregará el texto para que no modifique el diseño de las list)
+    const p = document.createElement("p");
+
+    
+    //Agregar el texto de la tarea (recibido por input)
+    p.textContent = task;
+    // Agregar el párrafo al li
+    li.appendChild(p);
+
+    //A cada li se le crean los dos botones de modificaciones
+    li.append(createButton("Delete","delete-btn"), createButton("Edit", "edit-btn"));
+        return li;
+}
+
+//CREAR LOS BOTONES DE CADA TAREA NUEVA
+//Parámetros el texto, y la clase
+function createButton(text, className) {
+    //Crea el nuevo botón 1 sola vez
+    const btn = document.createElement("button");
+    //Le agrega el texto al botón
+    btn.textContent = text;
+    //Le agrega la clase al botón
+    btn.className = className;
+    return btn;
+}
+
+
+//INTERACCIÓN CON BOTONES
+
+//Evento que viene desde el padre y él decide en qué botón ocurre el evento
+taskList.addEventListener("click", (event) =>{
+
+    //Valida si contiene la clase del botón para eliminar tareas
+    if(event.target.classList.contains("delete-btn")){
+        //LLAMADO a la función para REMOVER la tarea
+        deleteTask(event.target.parentElement);
+            //Valida si contiene la clase del botón para editar tareas
+    }else if(event.target.classList.contains("edit-btn")){
+        editTask(event.target.parentElement);
+    }
+})
+
+//taskItem: el parámetro seleccionado
+
+//Función para borrar elementos
+function deleteTask(taskItem){
+
+    //Valida con el usuario
+    if(confirm("¿Estás seguro de borrar esta tarea?")){
+        //REMUEVE EL ELEMENTOS
+        taskItem.remove();
+    }
+}
+
+//Función para editar elementos
+function editTask(taskItem){
+
+    //Valida con el usuario
+    //Me trae el contenido del texto que tiene la tarea para editarla
+    const editedTask = prompt("Edita tu tarea:", taskItem.querySelector("p").firstChild.textContent);
+
+    if(editedTask !== null){
+        //Si es diferente a null REESCRIBE EL CONTENIDO
+        taskItem.querySelector("p").firstChild.textContent = editedTask;
+    }
+}
