af7b5717d68e7545ab20073f5b0fd247c2a660d6
iamanyyeei <iamanyyeei@gmail.com>
2025-05-15 15:27:29 -0400

    Selección de elementos padre y búsqueda del valor de input en el form

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 //OBTENER LA INFORMACIÓN DEL FORMULARIO
+
+//Seleccionar el Formulario (Elemento Padre)
+const taskForm = document.getElementById("task-form");
+
+//Seleccionar el TaskList (Elemento Padre) donde se irán agregando todas las tareas
+const taskList = document.getElementById("task-list");
+
+//Agregar el evento de SUBMIY
+taskForm.addEventListener("submit", (event) =>{
+
+    //Prevenir el comportamiento por DEFAULT de refrescar la página en cada click al botón
+    event.preventDefault();
+
+    //Seleccionar el INPUT dentro del formulario
+    const taskInput = document.getElementById("task-input");
+
+    //Agarra el valor introducido en teclado por el usuario
+    const task = taskInput.value;
+    //Muestra dicho valor para confirmar que el código funciona hasta acá
+    console.log(task);
+    
+});
